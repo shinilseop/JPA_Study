7c6d391bfc36b32d221345dd6015f3635ff80fd4
shinilseop <shinilseop12@gmail.com>
2022-08-08 04:09:41 +0900

    init basic_jpa project

--- basic_jpa/src/main/java/Main.java
+++ basic_jpa/src/main/java/Main.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Main {
+import domain.Member;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Main {
+
+  public static void main(String[] args) {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+    EntityManager em = emf.createEntityManager();
+    EntityTransaction tx = em.getTransaction();
+
+    tx.begin();
+
+    Member member = new Member();
+    member.setId(3L);
+    member.setName("test");
+
+    em.persist(member);
+
+    Member result = em.find(Member.class, member.getId());
+    System.out.println("@@name:"+result.getName());
+
+    tx.commit();
+
+    em.close();
+
+    emf.close();
+  }
 }
